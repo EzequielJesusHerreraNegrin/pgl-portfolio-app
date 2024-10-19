6776c99374c4f3b821c28fa36942d51a18df360d
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-10-19 15:47:50 +0100

    fix: fix: I relocated the ScrollView elements to a recyclable component.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,9 @@
 import { useState } from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-} from "react-native";
+import { ScrollView, StyleSheet, View, Text } from "react-native";
 import Header from "./components/header/Header";
 import QRdisplay from "./components/qrDisplay/QRdisplay";
 import MyCard from "./components/myCard/MyCard";
+import ItemList from "./components/itemList/ItemList";
 
 export default function App() {
   const [displayMyQR, setDisplayMyQR] = useState<boolean>(false);
@@ -20,44 +15,9 @@ export default function App() {
         <View style={styles.bodystails}>
           <View>
             <MyCard></MyCard>
-            <Text
-              style={{
-                color: "beriblak",
-                fontWeight: "900",
-                textTransform: "capitalize",
-                fontSize: 20,
-                textAlign: "center",
-              }}
-            >
-              cosas que me gustan mucho:
-            </Text>
-            <ScrollView style={{ padding: 10 }}>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Salir a pasear
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Ir a la playita
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Domingos de misa
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                La guitarrita
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                El monte con lluvia
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Música variadita
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Ir de cenar romántica
-              </Text>
+            <Text style={styles.listTitle}>cosas que me gustan mucho:</Text>
+            <ScrollView>
+              <ItemList></ItemList>
             </ScrollView>
           </View>
         </View>
@@ -65,7 +25,7 @@ export default function App() {
         <QRdisplay></QRdisplay>
       )}
     </View>
-  );
+  );  
 }
 
 const styles = StyleSheet.create({
@@ -83,21 +43,11 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     height: "85%",
   },
-  avatar: {
-    height: 90,
-    width: 90,
-    borderRadius: 100,
-  },
-  cosasQmeGustanMuxoEstails: {
-    borderColor: "black",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    padding: 20,
-    color: "darkred",
+  listTitle: {
+    color: "veryblack",
+    fontWeight: "900",
+    textTransform: "capitalize",
+    fontSize: 20,
     textAlign: "center",
-    fontWeight: "bold",
-    fontStyle: "italic",
-    fontSize: 16,
-    backgroundColor: "silver",
   },
 });
